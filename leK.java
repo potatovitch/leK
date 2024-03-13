f9d0ffed6aedef69d46c97bd5866189d64a52a99
Yu_Neria <40469993+potatovitch@users.noreply.github.com>
2024-03-13 16:40:55 +0100

    Add files via upload

---
+++ leK.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+class leK{
+    String[] BULLSHIT_TAB = new String[]{"HI!", "ça c'est pas la question", "ça se voit qu'il n'y a pas d'investissment de votre coté", "Vous n'avez juste pas lu le document", "C'est vous qui vous créez des excuses", "vous n'avez pas posé de questions en amphi", "je serai jamais derrière vous, vous êtes pas des gamins", "tout le temps je le disais", "je ne suis pas votre parent"};
+
+    static String bullshit(){
+        Random rnd = new Random();
+        rnd = rnd.nextInt();
+        return BULLSHIT_TAB[rnd];
+    }
+
+
+    public static void main(String[] args){
+        while (true){
+            return bullshit();
+        }
+    }
+
+}
