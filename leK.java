5c37ccbc4af8cee259512c2bc45071edbb343193
Yu_Neria <40469993+potatovitch@users.noreply.github.com>
2024-03-13 16:56:14 +0100

    Update leK.java

--- leK.java
+++ leK.java
@@ -1,7 +1,18 @@
 import java.util.Random;
 
 class leK{
-    String[] BULLSHIT_TAB = new String[]{"HI!", "ça c'est pas la question", "ça se voit qu'il n'y a pas d'investissment de votre coté", "Vous n'avez juste pas lu le document", "C'est vous qui vous créez des excuses", "vous n'avez pas posé de questions en amphi", "je serai jamais derrière vous, vous êtes pas des gamins", "tout le temps je le disais", "je ne suis pas votre parent"};
+    String[] BULLSHIT_TAB = new String[]{"HI!",
+                                         "ça c'est pas la question", 
+                                         "ça se voit qu'il n'y a pas d'investissment de votre coté", 
+                                         "Vous n'avez juste pas lu le document", 
+                                         "C'est vous qui vous créez des excuses", 
+                                         "vous n'avez pas posé de questions en amphi", 
+                                         "je serai jamais derrière vous, vous êtes pas des gamins", 
+                                         "tout le temps je le disais", 
+                                         "je ne suis pas votre parent", 
+                                         "cette information, concrètement, je ne l'avais pas", 
+                                         "C'est vous qui me faites des reproches... pas moi",
+                                         "Je trouve ça bizare..."};
 
     static String bullshit(){
         Random rnd = new Random();
